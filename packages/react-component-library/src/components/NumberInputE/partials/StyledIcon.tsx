4eb1f410c2e92d690c143f7be5c06fc060736cd1
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-10-28 13:35:20 +0100

    fixup! feat(NumberInputE): Update to v3 design

--- packages/react-component-library/src/components/NumberInputE/partials/StyledIcon.tsx
+++ packages/react-component-library/src/components/NumberInputE/partials/StyledIcon.tsx
@@ -11,9 +11,9 @@ export const StyledIcon = styled.div`
   text-align: center;
   padding: ${spacing('6')} 0 ${spacing('6')} ${spacing('6')};
   order: 0;
+  color: ${color('neutral', '400')};
 
   & > svg {
-    color: ${color('neutral', '400')};
     height: 18px;
     width: 18px;
   }
